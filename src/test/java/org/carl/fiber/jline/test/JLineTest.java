d587d18e00ede3c9ff6c4f19faec9141e310783f
longjie <longjie@tydic.com>
2021-10-25 14:49:59 +0800

    修改 pom 中的包名称,去掉部分未使用内容

--- src/test/java/org/carl/fiber/jline/test/JLineTest.java
+++ src/test/java/org/carl/fiber/jline/test/JLineTest.java
@@ -1,11 +1,9 @@
 package org.carl.fiber.jline.test;
 
 import java.io.IOException;
-import java.net.URLEncoder;
 
 public class JLineTest {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(URLEncoder.encode("sE8iFO3ln8ml7@$c", "UTF-8"));
     }
 }
